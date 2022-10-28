ba8e21993663c19b69ffb48834b8c53509b44b40
Dijana Jovanovic <dijanabgjovanovic@gmail.com>
2022-10-28 07:56:39 +0200

    predavanje sesije, login

---
+++ undpPhpSesije/dbBroker.php
@@ -0,0 +1,13 @@
+<?php
+$baza = array(
+    array(
+        "id" => 1,
+        "username" => "admin",
+        "password" => "admin123"
+    ),
+    array(
+        "id" => 2,
+        "username" => "Dijana",
+        "password" => 123
+    )
+);
